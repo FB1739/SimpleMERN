fdc8d787ca6f65aceb9df01d5a803d12f423e00b
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-12-13 18:09:10 -0300

    segunda fase

--- mern-backend/config.js
+++ mern-backend/config.js
@@ -1,6 +1,11 @@
 const  config = {
     appConfig: {
         port: process.env.APP_PORT
+    },
+    db: {
+        port: process.env.DB_PORT,
+        dbName: process.env.DB_NAME,
+        host: process.env.DB_HOST
     }
 }
 
--- mern-backend/server.js
+++ mern-backend/server.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 const express = require("express")
 const connectDb = require('./db/mongodb')
-const {appConfig} = require("./config")
+const {appConfig, db} = require("./config")
 
 const app = express()
 
+connectDb(db)
 app.listen(appConfig.port, () => console.log(`listen on ${appConfig.port}`))
 
