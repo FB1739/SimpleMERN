1124eb1fab9f93565d882c0d60abd2bf17bfee5e
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-12-14 17:20:02 -0300

    siguiendo

--- mern-backend/config.js
+++ mern-backend/config.js
@@ -2,7 +2,7 @@ const config = {
     appConfig: {
         port: process.env.APP_PORT
     },
-    db: {
+    dbConfig: {
         port: process.env.DB_PORT,
         dbName: process.env.DB_NAME,
         host: process.env.DB_HOST
