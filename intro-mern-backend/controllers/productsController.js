2414a900e3f29889849f355ddad56c43c38ca4ab
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-12-16 16:04:24 -0300

    terminado el back, ahora por el front

--- intro-mern-backend/controllers/productsController.js
+++ intro-mern-backend/controllers/productsController.js
@@ -8,12 +8,19 @@ async function addProduct(req,res) {
             unitaryPrice,
             description
         } = req.body
+
         const product = Product({
             name,
             size,
             unitaryPrice,
             description
         })
+
+        if (req.file) {
+            const { filename } = req.file
+            product.setImgUrl(filename)
+        }
+
         const productStored = await product.save()
 
         res.status(201).send({productStored})
@@ -23,6 +30,12 @@ async function addProduct(req,res) {
     
 }
 
+async function getProducts(req, res) {
+    const products = await Product.find().lean().exec()
+    res.status(200).send( {products} )
+}
+
 module.exports = {
-    addProduct
+    addProduct,
+    getProducts
 }
---
+++ intro-mern-backend/libs/storage.js
@@ -0,0 +1,18 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './storage/imgs')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      //cb(null, file.fieldname + '-' + uniqueSuffix) es lo mismo que lo da abajo pero con template strings
+      cb(null, `${file.fieldname}-${uniqueSuffix}.png`)
+    
+    }
+  })
+  
+  const upload = multer({ storage })
+  
+
+  module.exports = upload
