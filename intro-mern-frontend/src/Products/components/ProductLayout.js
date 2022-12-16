d11cf6deef99196d3c42342baf398907f49a5cb2
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-12-16 17:32:04 -0300

    nada

---
+++ intro-mern-frontend/src/Products/components/ProductLayout.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const ProductLayout = () => {
+    return "ProductLayout"
+}
+
+export default ProductLayout
--- intro-mern-frontend/src/index.js
+++ intro-mern-frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ProductLayout from './Products/components/ProductLayout';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductLayout  />
   </React.StrictMode>
 );
 
