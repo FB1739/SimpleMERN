a7c8d703c804e01ae9bb6109b845722e89cf4502
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-12-22 18:01:48 -0300

    arregle eso al final

--- intro-mern-frontend/src/Products/components/ProductLayout.js
+++ intro-mern-frontend/src/Products/components/ProductLayout.js
@@ -2,18 +2,23 @@ import React, { useState } from "react"
 import Header from './Header'
 import AddButton from './AddButton'
 import Loading from './Loading'
-import { Button } from 'react-bulma-components'
+
 
 const ProductLayout = () => {
-    const  {isLoading, setIsLoading} = useState(true)
+    const  [isLoading, setIsLoading] = useState(true)
+    //const state = useState(true)
     return ( 
     <>
         <Header title="Products app"/>
         <AddButton/>
-        <Button onClick={ () => setIsLoading(false) }>update</Button>
-        <Loading />
+        
+        {
+            isLoading
+            ? <Loading />
+            : "huehue"
+        }
+        
     </>
-    
     )
 
 }
